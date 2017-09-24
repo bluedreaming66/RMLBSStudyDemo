72e2626cfb912d4518a5195349ff79f72c226643
blue <renmengli@Macintosh.local>
2017-09-24 17:25:11 +0800

    初始化项目

---
+++ BSBDJDemo/BSBDJDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BSBDJDemo
+//
+//  Created by blue on 2017/9/24.
+//  Copyright © 2017年 blue. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ BSBDJDemo/BSBDJDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BSBDJDemo
+//
+//  Created by blue on 2017/9/24.
+//  Copyright © 2017年 blue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
